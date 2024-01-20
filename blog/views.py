fdee4621b784f36b25e5a2cba82af8a8685273b8
Brian Koome <b61240@gmail.com>
2024-01-20 08:53:49 +0300

    First Commit

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts =[
+    {
+        'author' : 'Brian K',
+        'title' : 'Blog post 1',
+        'Content': 'Our first post',
+        'date_posted' : '09 January 2024'
+
+    },
+    {
+        'author' : 'Vanessa M',
+        'title' : 'Blog post 2',
+        'Content': 'Our second post',
+        'date_posted' : '09 February 2024'
+    }
+]
+# Create your views here.
+
+def home (request): #  we will handle the request argument later
+    context= {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about (request): #  we will handle the request argument later
+    return render(request, 'blog/about.html', {'title': 'About'} ) 

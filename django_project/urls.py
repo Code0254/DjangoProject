26225c7187be88a3c2031c905f9c29f8e180facf
Brian Koome <b61240@gmail.com>
2024-01-27 20:29:09 +0300

    Added Users

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views  # import login and logout view from django. When importing views use 'as' since you will be importing multiple views
 from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name= 'register') ,
-    path('blog/', include('blog.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name= 'login'),  # Class based view. Note the use of ' .as_view() '
+    path('logout/', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name= 'logout'),  # When referring to link name: {{ logout }}
+    path('', include('blog.urls')),
 
 ]
+
--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages # flash message. The message is displayed once and will dissapear on the next request
 from django.contrib.auth.forms import UserCreationForm # Django way (Class) of creating the registration form 
 
 # Create your views here.
 def register (request):
-    form = UserCreationForm
-    return render (request, 'users/register.html', {'form': form}) # request, template, form
+    if request.method == 'POST':  # Form is submitted as a post request
+        form = UserCreationForm(request.POST)  # Create a new form that has the data in the post. 'Username and two password fields'
+        if form.is_valid():  # Check multiple django conditions like, does the user exist? Do the passwords match? If invalid, it returns the render
+            form.save()  # Save the user
+            username = form.cleaned_data.get('username')  # Grab the username
+            messages.success(request, f'Account created for {username}!')  # Our flash message
+            return redirect ('blog-home')  # Note the absence of render
+    else:
+        form = UserCreationForm()
+    return render (request, 'users/register.html', {'form': form})  # Render our register.html document with a form
 
+'''
+
+Types of messages:
+message.debug
+message.info
+message.success
+message.warning
+message.error
+
+'''
